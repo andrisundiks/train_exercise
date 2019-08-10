cf164cbded351a72ce32aeac26db2a9e820d5a3f
n <not@provided.nil>
2019-08-10 10:08:29 +0100

    Form populated dynamically and controlled.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 
+import { trainRoutes } from "./trainRoutes";
+
 function App() {
+   const [ origin, setOrigin ] = useState('Edinburgh');
+   const [ destination, setDestination ] = useState('Edinburgh');
+
+   // Creates array of jsx objects from trainRoutes to populate our select elements
+   const generateCitiesList = () => {
+       return Object.keys(trainRoutes).map( city => {
+           return <option value={city}> {city} </option>
+       })
+   };
+
+   const handleOrigin = e => { setOrigin(e.target.value) };
+   const handleDestination = e => { setDestination(e.target.value) };
+
   return (
       <section className="hero is-success is-fullheight">
         <div className="hero-body">
@@ -9,27 +24,35 @@ function App() {
             <div className="columns">
               <div className="column is-one-fifth">
                   <section className="is-pulled-right">
+                      <label htmlFor="origin">Select an origin</label>
                       <div className="field">
                           <div className="control">
                               <select
                                   name="origin"
                                   className="select is-info"
+                                  value={ origin }
+                                  onChange={ handleOrigin }
                               >
+                                  { generateCitiesList() }
                               </select>
                           </div>
                       </div>
+                      <label htmlFor="origin">Select a destination</label>
                       <div className="field">
                           <div className="control">
                               <select
                                   name="destination"
                                   className="select is-info"
+                                  value={ destination }
+                                  onChange={ handleDestination }
                               >
+                                  { generateCitiesList() }
                               </select>
                           </div>
                       </div>
                   </section>
               </div>
-              <div className="column box">Auto</div>
+              <div className="column is-two-thirds box">Auto</div>
             </div>
           </div>
         </div>
---
+++ src/trainRoutes.js
@@ -0,0 +1,15 @@
+
+export const trainRoutes = {
+        Edinburgh: {
+            Glasgow: true,
+            London: true,
+        },
+        London: {
+            Edinburgh: true,
+            Glasgow: true,
+            Liverpool: true,
+        },
+        Liverpool: {
+            London: true,
+        }
+};
